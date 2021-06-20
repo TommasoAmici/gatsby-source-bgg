6982af38f2d93829c16952fa7c434090398d9641
Tommaso Amici <tommaso.amici@me.com>
2021-06-20 12:29:50 +0200

    Run prettier after update to 2.3.1

--- src/types.ts
+++ src/types.ts
@@ -2,20 +2,23 @@ type BuildPowersOf2LengthArrays<N extends number, R extends never[][]> = R[0][N]
   ? R
   : BuildPowersOf2LengthArrays<N, [[...R[0], ...R[0]], ...R]>;
 
-type ConcatLargestUntilDone<N extends number, R extends never[][], B extends never[]> =
-  B["length"] extends N
-    ? B
-    : [...R[0], ...B][N] extends never
-    ? ConcatLargestUntilDone<
-        N,
-        R extends [R[0], ...infer U] ? (U extends never[][] ? U : never) : never,
-        B
-      >
-    : ConcatLargestUntilDone<
-        N,
-        R extends [R[0], ...infer U] ? (U extends never[][] ? U : never) : never,
-        [...R[0], ...B]
-      >;
+type ConcatLargestUntilDone<
+  N extends number,
+  R extends never[][],
+  B extends never[]
+> = B["length"] extends N
+  ? B
+  : [...R[0], ...B][N] extends never
+  ? ConcatLargestUntilDone<
+      N,
+      R extends [R[0], ...infer U] ? (U extends never[][] ? U : never) : never,
+      B
+    >
+  : ConcatLargestUntilDone<
+      N,
+      R extends [R[0], ...infer U] ? (U extends never[][] ? U : never) : never,
+      [...R[0], ...B]
+    >;
 
 type Replace<R extends any[], T> = { [K in keyof R]: T };
 
