fc02a5547ad2735f29028772a80b678202d0d508
Tommaso Amici <me@tommasoamici.com>
2022-09-30 20:02:49 +0200

    feat!: updated plugin for gatsby 4

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -4,51 +4,62 @@ import { fetchCollection } from "./index";
 
 const GAME_NODE_TYPE = "BggGame";
 
-export const sourceNodes = async (args: SourceNodesArgs, pluginOptions: ICollectionParams) => {
-  const activity = args.reporter.activityTimer("gatsby-source-bgg");
+export const sourceNodes = async (
+  { reporter, cache, createNodeId, actions, createContentDigest }: SourceNodesArgs,
+  pluginOptions: ICollectionParams
+) => {
+  const activity = reporter.activityTimer("gatsby-source-bgg");
   activity.start();
+
   const cacheKey = "gatsby-source-bgg";
-  let sourceData: ICollection[] = await args.cache.get(cacheKey);
+  let sourceData: ICollection[] = await cache.get(cacheKey);
   if (!sourceData) {
     sourceData = await fetchCollection(pluginOptions);
-    await args.cache.set(cacheKey, sourceData);
+    await cache.set(cacheKey, sourceData);
   }
 
-  sourceData.forEach((game: ICollection) => {
-    const nodeMeta = {
-      id: args.createNodeId(`${GAME_NODE_TYPE}-${game.objectID}`),
+  for (const game of sourceData) {
+    const id = createNodeId(`${GAME_NODE_TYPE}-${game.objectID}`);
+    const node: NodeInput = {
+      ...game,
+      id,
       parent: null,
       children: [],
       internal: {
         type: GAME_NODE_TYPE,
         mediaType: `text/json`,
         content: JSON.stringify(game),
-        contentDigest: args.createContentDigest(game),
+        contentDigest: createContentDigest(game),
       },
     };
-    const node: NodeInput = { ...game, ...nodeMeta };
-    args.actions.createNode(node);
-  });
+    await actions.createNode(node);
+  }
+
   activity.setStatus(`Nodes created for ${sourceData.length} games`);
   activity.end();
   return;
 };
 
 // called each time a node is created
-export const onCreateNode = async (args: CreateNodeArgs) => {
-  if (args.node.internal.type === GAME_NODE_TYPE) {
+export const onCreateNode = async ({
+  node,
+  cache,
+  getCache,
+  actions: { createNode, createNodeField },
+  createNodeId,
+}: CreateNodeArgs) => {
+  if (node.internal.type === GAME_NODE_TYPE) {
     const remoteFileNodeArgs: CreateRemoteFileNodeArgs = {
-      url: String(args.node.image),
-      parentNodeId: args.node.id,
-      store: args.store,
-      cache: args.cache,
-      createNode: args.actions.createNode,
-      createNodeId: args.createContentDigest,
-      reporter: args.reporter,
+      url: String(node.image),
+      cache,
+      getCache,
+      parentNodeId: node.id,
+      createNode,
+      createNodeId,
     };
     const fileNode = await createRemoteFileNode(remoteFileNodeArgs);
     if (fileNode) {
-      args.node.coverImage = fileNode.id;
+      createNodeField({ node, name: "coverImage", value: fileNode.id });
     }
   }
 };
@@ -57,6 +68,6 @@ export const createSchemaCustomization = async (args: CreateSchemaCustomizationA
   const { createTypes } = args.actions;
   createTypes(`
     type ${GAME_NODE_TYPE} implements Node {
-      coverImage: File @link
+      coverImage: File @link(from: "fields.coverImage")
     }`);
 };
